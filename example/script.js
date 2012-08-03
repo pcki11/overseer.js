9a17ae02a058d26d41b5f3ef6dfbb27cd52878cd
libra guest <realgodd@gmail.com>
2012-08-03 15:23:30 -0400

    first iteration

---
+++ example/script.js
@@ -0,0 +1,64 @@
+/**
+ * Test
+ */
+Overseer.create(function(){
+    var max = 0,
+        start = 1,
+        numArray = [],
+        resEl = $("#res"),
+        stopEl = $("#stop"),
+        startEl = $("#start"),
+        updateNumBar = function(number, val){
+            var el = $("#num" + number);
+            numArray[number] = val;
+            if(numArray[number] > max){
+                max = numArray[number];
+                for(var i in numArray){
+                    var elToUpdate = $("#num" + i);
+                    elToUpdate.setAttribute("style","height:" + ((numArray[i] / max) * 100) + "px;");
+                }
+            }
+            el.innerHTML = numArray[number];
+            el.setAttribute("style","height:" + ((numArray[number] / max) * 100) + "px;");
+        };
+    var progress = function(diff){
+        var c = 1, sign = 1;
+        for(var i=start;i<start + 2*diff;i++){
+            if(i > 0 && i < 201 && c > 0){
+                updateNumBar(i, c % diff);
+            }
+            c += sign;
+            if(c == diff){
+                sign = -1;
+                c--;
+            }
+        }
+        if(start < 201){
+            start++;
+        }
+        else
+            start = -diff;
+    }
+    for(var j = 1; j < 201; j++){
+        var nCont = document.createElement("div"),
+            el = document.createElement("div");
+        el.setAttribute("id", "num" + j);
+        el.innerHTML = 0;
+        el.setAttribute("style","height:100px;");
+        nCont.innerHTML = j;
+        nCont.appendChild(el);
+        resEl.appendChild(nCont);
+        numArray[j] = 0;
+    }
+    
+    stopEl.addEventListener("click", function(){
+        Overseer.stop();
+    }, false);
+    
+    startEl.addEventListener("click", function(){
+        Overseer.create(function(){
+            progress(6);
+        }, {delay: 40});
+    }, false);
+    
+}, {delay: 1000, single: true});
---
+++ overseer.js
@@ -0,0 +1,198 @@
+
+/**
+ * Engine
+ */
+var Overseer;
+(function(){
+    window.$ = function(selector){
+        if(!selector)
+            return null;
+		var DOMobj = null;
+		if(typeof selector === "string"){
+            var elArray = selector.split(" ");
+            for(var i in elArray){
+                var item = elArray[i];
+                switch(item[0]){
+                    case ".":
+                        //TODO get elements by class
+                        break;
+                    case "#":
+                        DOMobj = document.getElementById(item.substring(1));
+                        break;
+                    default:
+                        
+                        break;
+                }
+            }
+		}
+        else if(typeof selector === "object"){
+            if(selector === document){
+                console.dir(this);
+                return;
+            }
+        }
+        return DOMobj;
+	};
+    var ThreadManager = function(){
+        
+        /**
+         * Init
+         **/
+        var settings = {
+            errors: {
+                mainArgumentsPassed: "ThreadManager is not a function, use ThreadManager.create(func) instead.",
+                internalMissingType: "Missing type: ",
+                createThreadCreationFailed: "Thread creation failed.",
+                stopThreadNotFound: "No thread with such id: "
+            },
+            infos: {
+                createEmptyCreate: "Create function called with no arguments, nothing created.",
+                typeNotImplemented: "Type not implemented: "
+            }
+        };
+        
+        if(arguments.length > 0){
+            console.error(settings.errors.mainArgumentsPassed);
+            return;
+        }
+        
+        /**
+         * Helpers
+         **/
+        var getName = function(n){
+            for(var i in n){
+                return i;
+            }
+        };
+        var isNothing = function(obj){
+            return  (obj === null) ||
+                    (obj === undefined);
+        };
+        var type = function(obj){
+            if(obj instanceof Function) return "function";
+            if(obj instanceof Number) return "number";
+            if(obj instanceof Boolean) return "boolean";
+            if(obj instanceof String) return "string";
+            if(obj instanceof Array) return "array";
+            if(obj instanceof Date) return "date";
+            if(obj.toString().indexOf("Object") != -1) return "object";
+            console.info(settings.infos.typeNotImplemented + obj.toString());
+            return obj.toString();
+        };
+        
+        /**
+         * Threads class
+         **/
+        var Threads = function(){
+            var threads = {};
+            this.push = function(obj){
+                threads[obj.name] = obj;
+                obj.start();
+                return obj.name;
+            };
+            this.stop = function(str){
+                var thread = threads[str];
+                if(thread === undefined){
+                    throw new Error("No such thread");
+                }
+                thread.stop();
+            };
+            this.stopAll = function(){
+                for(var i in threads){
+                    threads[i].stop();
+                }
+            };
+        };
+        var threads = new Threads();
+        
+        /**
+         * Thread class
+         **/
+        var Thread = function(name, func, opt){
+            var delay = 500;
+            if(!isNothing(opt.delay)){
+                delay = opt.delay;
+            }
+            var stopped = true,
+                timer = null;
+            var getDelay = function(){
+                if(delay === "random"){
+                    return Math.floor(Math.random() * 1000 + 1);
+                }
+                return delay;
+            };
+            var run = function(){
+                if(!stopped){
+                    if(!opt.single)
+                        timer = setTimeout(arguments.callee, getDelay());
+                    func.call();
+                }
+            };
+            this.name = name;
+            this.start = function(){
+                stopped = false;
+                timer = setTimeout(run, getDelay());
+            };
+            this.stop = function(){
+                stopped = true;
+                if(!isNothing(timer)){
+                    clearTimeout(timer);
+                }
+            };
+        };
+        
+        /**
+         * Interface
+         **/
+        var anonThreads = 0;
+        this.create = function(){
+            var arg = arguments[0],
+                objType = type(arg),
+                threadID = null;    
+            var opt = {};
+            if(arguments.length < 1) {
+                console.info(settings.infos.createEmptyCreate);
+                return;
+            }
+            if(arguments.length > 1)
+                if(!isNothing(arguments[1]) && type(arguments[1]) === "object"){
+                    opt = arguments[1];
+                }
+            if(isNothing(opt.single)){
+                opt.single = false;
+            }
+            if(objType === "function"){
+                threadID = arg.name;
+                if(threadID.length === 0){
+                    threadID = "_thread" + (anonThreads++);
+                }
+                threadID = threads.push(new Thread(threadID, arg, opt));
+            }
+            if(threadID === null){
+                console.error(settings.errors.createThreadCreationFailed);
+            }
+            return threadID;
+        };
+        this.stop = function(){
+            var threadID = null;
+            if(arguments.length > 0)
+                threadID = arguments[0];
+            if(threadID === null){
+                threads.stopAll();
+                return;
+            }
+            try {
+                threads.stop(threadID);
+            }
+            catch(ex){
+                console.error(settings.errors.stopThreadNotFound + threadID);
+            }
+        };
+	};
+    
+    /**
+     * Start init
+     **/
+    Overseer = new ThreadManager();
+    
+})();
